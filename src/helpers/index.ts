c9b85e5420cc62209734164b27fcfd0671338b16
JenieX <sozorablack@gmail.com>
2023-05-29 02:04:40 +0300

    ✨ Add new helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,4 @@ export { $, $$ } from './query-selector';
 export { default as addStyle } from './add-style';
 export { default as fish } from './fish';
 export { default as sleep } from './sleep';
+export { default as waitForImageLoad } from './wait-for-image-load';
