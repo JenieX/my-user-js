4f1e10a3253cd53e26dbfdc6f769ee68e1cc022e
JenieX <sozorablack@gmail.com>
2023-08-11 14:24:48 +0300

    ✅ Update version to 0.0.3

--- dist/lb-common-films-alt/lb-common-films-alt.meta.js
+++ dist/lb-common-films-alt/lb-common-films-alt.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           jx-letterboxd-common-films-alt
-// @version        0.0.2
+// @version        0.0.3
 // @namespace      https://github.com/JenieX/user-js
 // @description    Discover users on Letterboxd with similar movie tastes
 // @author         JenieX
