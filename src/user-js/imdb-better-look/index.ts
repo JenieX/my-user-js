9c8256c6aa1ee5a5518b37b6182d05585f1f0fe1
JenieX <sozorablack@gmail.com>
2023-08-30 10:00:29 +0300

    🎉 Begin the project

---
+++ src/user-js/imdb-better-look/index.ts
@@ -0,0 +1,7 @@
+import { addStyle } from '../../helpers';
+
+addStyle('include-file: style.min.css');
+
+// include
+// /https:\/\/www\.imdb\.com\/title\/[^/]+\/(\?.+)?$/
+// /https:\/\/www\.imdb\.com\/(title|name)\/[^/]+\/(\?.+)?$/
