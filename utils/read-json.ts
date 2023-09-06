00749f53070a807703caa8a421818465bcddb280
JenieX <sozorablack@gmail.com>
2023-09-06 08:21:56 +0300

    🧹 Update config files

--- utils/read-json.ts
+++ utils/read-json.ts
@@ -1,7 +1,6 @@
 import fsp from 'node:fs/promises';
 
-async function readJSON<T= Record<string, any>>(fileAbsolutePath: string): Promise<T> {
-  // eslint-disable-next-line unicorn/prefer-json-parse-buffer
+async function readJSON<T = object>(fileAbsolutePath: string): Promise<T> {
   const metadataString = await fsp.readFile(fileAbsolutePath, 'utf-8');
 
   return JSON.parse(metadataString) as T;
